3d937d9e4cc4130994d51b4bfd95c7bf6f3081ca
AlexVipond <alex.r.vipond@gmail.com>
2018-05-30 13:56:33 -0500

    Scrape country data

--- one-acre-fund/scrape-insights.js
+++ one-acre-fund/scrape-insights.js
@@ -11,14 +11,26 @@ async function scrapePages() {
     return num;
   }, 0);
 
-  console.log(pageTotal);
+  let countries = await page.evaluate(results => {
+    let nodes = document.querySelectorAll('.filters__children__item__link');
+
+    nodes.forEach(node => {
+      let country = new Object();
+      country.link = node.href;
+      country.name = node.querySelector('.filters__children__item__heading').textContent;
+      results.push(country);
+    });
+
+    results = results.filter(object => /country/.test(object.link) === true);
+    return results;
+  }, []);
 
   let insights = [];
 
   for (i = 0; i < pageTotal; i++) {
     let page2 = await browser.newPage();
     await page2.goto('https://oneacrefund.org/insights-library/?page=' + (i + 1));
-    let cards = await page.evaluate(results => {
+    let cards = await page2.evaluate(results => {
       let fields = {
         'label': '.cards__item__heading',
         'element type': '.cards__item__image',
@@ -65,7 +77,9 @@ async function scrapePages() {
           'label': getLabel(node),
           'element type': getType(node),
           'description': getDescription(node),
-          'link': getLink(node)
+          'link': getLink(node),
+          'id': getLink(node),
+          'countries': []
         });
       });
 
@@ -81,6 +95,28 @@ async function scrapePages() {
 
   console.log(insights.length);
 
+  for(i = 0; i < countries.length; i++) {
+    let page3 = await browser.newPage();
+    await page3.goto(countries[i].link);
+    insights = await page3.evaluate((insights, countryName) => {
+      let nodes = document.querySelectorAll('.cards__item__link');
+      let cardLinks = [];
+      nodes.forEach(node => {
+        cardLinks.push(node.href);
+      });
+
+      insights.forEach(insight => {
+        if(cardLinks.findIndex(link => link === insight.link) > -1) {
+          insight.countries.push(countryName);
+        }
+      });
+
+      return insights;
+    }, insights, countries[i].name);
+
+    await page3.close();
+  }
+
   function createElementBlueprint(elementsArray) {
       let elementBlueprint = JSON.parse(
           "{\"elements\":" + JSON.stringify(elementsArray) + "}"
@@ -92,6 +128,8 @@ async function scrapePages() {
     if (err) throw err;
     console.log('Map those insights!');
   });
+
+  await browser.close();
 }
 
 scrapePages();
