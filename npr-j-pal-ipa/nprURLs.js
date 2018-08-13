6d5accf9ddbfaef390f6dea05f91628763b97986
AlexVipond <alex.r.vipond@gmail.com>
2018-08-12 23:54:19 -0500

    npr jpal urls

---
+++ npr-j-pal-ipa/nprURLs.js
@@ -0,0 +1,7 @@
+let npr = [
+'https://www.npr.org/search?query=%22j-pal%22&page=1',
+'https://www.npr.org/search?query=%22dean%20karlan%22&page=1',
+'https://www.npr.org/search?query=%22abhijit%20banerjee%22&page=1',
+'https://www.npr.org/search?query=%22esther%20duflo%22&page=1',
+'https://www.npr.org/search?query=%22innovations%20for%20poverty%20action%22&page=1'
+]
